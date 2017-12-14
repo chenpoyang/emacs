f12dfebc046df0acabf1ebd06ffa34ec453961f5
Peck Chen <peck.cpy@gmail.com>
2017-12-14 11:52:50 +0800

    add eshell fancy prompt

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -46,6 +46,26 @@
 ;;disable startup splash screen
 ;;(setq inhibit-startup-screen t)
 "---------------------------------------------------------------------------"
+;; eshell prompt
+(defmacro with-face (str &rest properties)
+     `(propertize ,str 'face (list ,@properties)))
+
+(defun shk-eshell-prompt ()
+  (let ((header-bg "#fff"))
+    (concat
+     (with-face (concat (eshell/pwd) " ") :background header-bg)
+     (with-face (format-time-string "(%Y-%m-%d %H:%M) " (current-time)) :background header-bg :foreground "#888")
+     (with-face
+      (or (ignore-errors (format "(%s)" (vc-responsible-backend default-directory))) "")
+      :background header-bg)
+     (with-face "\n" :background header-bg)
+     (if (= (user-uid) 0)
+	 (with-face "#" :foreground "red")
+       (with-face "$" :foreground "green"))
+     " ")))
+(setq eshell-prompt-function 'shk-eshell-prompt)
+(setq eshell-highlight-prompt nil)
+"---------------------------------------------------------------------------"
 ;;Insert-current-date
 (defun insert-current-date ()
   (interactive "*")
