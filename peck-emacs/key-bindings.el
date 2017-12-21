c25a32d54d6240dd8bf076c550d2df72aa1d50b4
Peck Chen <peck.cpy@gmail.com>
2017-12-21 15:59:31 +0800

    move "C-x j" "C-x l" key-bindinds to key-bindings.el

--- peck-emacs/key-bindings.el
+++ peck-emacs/key-bindings.el
@@ -1,5 +1,7 @@
 (global-set-key (kbd "s-D") 'dired)
 (global-set-key (kbd "C-h C-f") 'find-function-at-point)
 (global-set-key (kbd "C-h C-v") 'find-variable-at-point)
+(global-set-key (kbd "C-x j") 'previous-buffer)
+(global-set-key (kbd "C-x l") 'next-buffer)
 (global-set-key "\C-cV" 'visit-tags-table)
 (global-set-key (kbd "C-c d r") 'recentf-edit-list)
