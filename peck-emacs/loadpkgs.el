43677de5307214326beeb3e7acc1e91ac09f38a7
Peck Chen <peck.cpy@gmail.com>
2017-12-23 17:13:42 +0800

    add markdown-mode, smartparens features

--- peck-emacs/loadpkgs.el
+++ peck-emacs/loadpkgs.el
@@ -668,3 +668,14 @@
 "---------------------------------------------------------------------------"
 (use-package try)
 "---------------------------------------------------------------------------"
+(use-package markdown-mode
+  :commands (markdown-mode gfm-mode)
+  :mode (("README\\.md\\'" . gfm-mode)
+         ("\\.md\\'" . markdown-mode)
+         ("\\.markdown\\'" . markdown-mode))
+  :init (setq markdown-command "multimarkdown"))
+"---------------------------------------------------------------------------"
+(use-package smartparens
+  :init
+  (add-hook 'prog-mode-hook 'smartparens-mode))
+"---------------------------------------------------------------------------"
