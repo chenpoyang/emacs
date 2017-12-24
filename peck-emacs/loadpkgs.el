1750546eb60dbf6021db0423a9b9b138e5ac3660
Peck Chen <peck.cpy@gmail.com>
2017-12-24 13:48:05 +0800

    (evil-set-initial-state-mode 'inferior-python-mode 'emacs)

--- peck-emacs/loadpkgs.el
+++ peck-emacs/loadpkgs.el
@@ -562,7 +562,8 @@
   (dolist (mode '(eshell-mode shell-mode term-mode terminal-mode compilation-mode
                               comint-mode erc-mode special-mode gud-mode dired-mode
                               git-commit-mode diff-mode custom-mode all-the-icons-dired-mode
-                              dired-mode calendar-mode help-mode Info-mode wdired-mode))
+                              dired-mode calendar-mode help-mode Info-mode wdired-mode
+                              inferior-python-mode))
     (evil-set-initial-state mode 'emacs)))
 "---------------------------------------------------------------------------"
 ;; projectile
